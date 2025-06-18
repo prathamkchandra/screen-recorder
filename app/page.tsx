6a058e63036dcaa0e3833c4b4974a6897914540d
prathamkchandra <prathamkappu565@gmail.com>
2025-06-18 21:44:58 +0530

    initalized project with required packages

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='justify-center'>
+      
+      <h1>Helo welcome</h1>
+      </div>  
+  )
+}
+
+export default Page
