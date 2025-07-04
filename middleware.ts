ddee7bd45ed6d29dd206afc343ca65e7d7960da9
prathamkchandra <prathamkappu565@gmail.com>
2025-07-05 00:47:47 +0530

    fixed bugs

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth";
-import aj, { createMiddleware, detectBot, shield } from "./lib/arcjet";
 
-export async function middleware(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) {
+export function middleware(request: NextRequest) {
+  const isLoggedIn = request.cookies.get("auth-token");
+  
+  if (!isLoggedIn) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
 }
-const validate = aj
-  .withRule(
-    shield({ 
-      mode: "LIVE",
-    })
-  )
-  .withRule(
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER"], // allow other bots if you want to.
-    })
-  );
-
-export default createMiddleware(validate);
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
 };
-
-// ⨯ [TypeError: Body is unusable: Body has already been read]
