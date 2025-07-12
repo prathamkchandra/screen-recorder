081d190480e7c5a64c789cdfdba693e76f0efc4c
DhyanTD <dhyan@smoketrees.in>
2025-07-12 19:54:15 +0530

    fix: next js config

--- next.config.ts
+++ next.config.ts
@@ -3,32 +3,47 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-},
-typescript: {
-  ignoreBuildErrors: true,
-},
-images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "lh3.googleusercontent.com",
-      port: "",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "prat-screencast.b-cdn.net",
-      port: "",
-      pathname: "/**",
-    },
-  ],
-},
-
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "prat-screencast.b-cdn.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
     },
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
